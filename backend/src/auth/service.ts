b45896c115729a72439774462f93dc7192b816a1
moshfiqrony <moshfiqrony@gmail.com>
2021-05-13 18:57:11 +0600

    MR | Auth service

---
+++ backend/src/auth/service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/users/service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.userService.findOne(username, password);
+    if (user) {
+      return user;
+    } else {
+      return null;
+    }
+  }
+
+  async login(user: any) {    
+    const payload = { username: user.data.username, sub: user.data.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
