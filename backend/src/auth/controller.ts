31d804af365346214ef22fe03c9b67c99a9f5c33
moshfiqrony <moshfiqrony@gmail.com>
2021-05-13 23:40:51 +0600

    MR | Route convention added

--- backend/src/auth/controller.ts
+++ backend/src/auth/controller.ts
@@ -1,15 +1,29 @@
-import {Controller, Post, Req, UseGuards} from '@nestjs/common'
-import { AuthGuard } from '@nestjs/passport'
-import { Request } from 'express'
+import { Controller, Post, Query, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+import { UserService } from 'src/users/service';
 import { AuthService } from './service';
 
-@Controller()
-export class AuthController{
-    constructor(private authService: AuthService){}
+@Controller('api/auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
 
-    @UseGuards(AuthGuard('local'))
-    @Post('auth/login')
-    async login(@Req() req: Request){
-        return this.authService.login(req.user);
-    }
+  @Post('register')
+  create(
+    @Req() request: Request,
+    @Query('email') email: string,
+    @Query('username') username: string,
+    @Query('password') password: string,
+  ) {
+    return this.userService.create(email, username, password);
+  }
+
+  @UseGuards(AuthGuard('local'))
+  @Post('login')
+  async login(@Req() req: Request) {
+    return this.authService.login(req.user);
+  }
 }
--- backend/src/users/controller.ts
+++ backend/src/users/controller.ts
@@ -12,23 +12,13 @@ import { JWTAuthGuard } from 'src/auth/JWTAuthGuard';
 import { TokenVerifyingGuard } from 'src/auth/TokenVerifyingGuard';
 import { UserService } from './service';
 
-@Controller('user')
+@Controller('/api/me')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  create(
-    @Req() request: Request,
-    @Query('email') email: string,
-    @Query('username') username: string,
-    @Query('password') password: string,
-  ) {
-    return this.userService.create(email, username, password);
-  }
-
   @UseGuards(TokenVerifyingGuard)
   @UseGuards(JWTAuthGuard)
-  @Get('me')
+  @Get()
   get(@Req() request: any) {
     return this.userService.findOneById(request.user.id);
   }
