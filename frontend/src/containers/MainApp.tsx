744dcfa2276f569615a84348def4f12484cc6a7e
moshfiqrony <moshfiqrony@gmail.com>
2021-05-15 00:15:24 +0600

    MR | React-query usage improved

--- frontend/src/containers/MainApp.tsx
+++ frontend/src/containers/MainApp.tsx
@@ -4,69 +4,88 @@ import LoginForm from "./Login";
 import RegisterForm from "./Register";
 
 const ClientApp = () => {
-    const { data, error, isLoading } = useQuery("user", () =>
+  const { data, error, isLoading } = useQuery({
+    queryKey: 'user',
+    queryFn: () =>
       fetch(`http://localhost:8000/api/me`, {
         method: "GET",
         headers: {
           authorization: `Bearer ${localStorage.getItem("token")}`,
         },
-      }).then((res) => res.json())
-    );
-  
-    const handleRegister = (
-      username: string,
-      password: string,
-      email: string
-    ) => {
-      fetch(
-        `http://localhost:8000/api/auth/register?username=${username}&email=${email}&password=${password}`,
-        {
-          method: "POST",
-        }
-      )
-        .then((res) => res.json())
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => console.log(err));
-    };
-  
-    const handleLogin = (username: string, password: string) => {
-      fetch(
-        `http://localhost:8000/api/auth/login?username=${username}&password=${password}`,
-        {
-          method: "POST",
-        }
-      )
-        .then((res) => res.json())
-        .then((res) => {
-          localStorage.setItem("token", res.access_token);
-        })
-        .catch((err) => console.log(err));
-    };
-  
-    const state = data?.data;
-  
-    if (isLoading) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <Fragment>
-          {state.username && <div>{state.username}</div>}
-          {state.username === null && (
-            <div>
-              <RegisterForm onSubmit={handleRegister} />
-            </div>
-          )}
-          {state.username === null && (
-            <div>
-              <LoginForm onSubmit={handleLogin} />
-            </div>
-          )}
-        </Fragment>
-      );
-    }
+      }).then((res) => res.json()),
+      retry: false,
+      refetchOnWindowFocus: false
+  });
+
+  const handleRegister = (
+    username: string,
+    password: string,
+    email: string
+  ) => {
+    fetch(
+      `http://localhost:8000/api/auth/register?username=${username}&email=${email}&password=${password}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
   };
 
+  const handleLogin = (username: string, password: string) => {
+    fetch(
+      `http://localhost:8000/api/auth/login?username=${username}&password=${password}`,
+      {
+        method: "POST",
+      }
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        localStorage.setItem("token", res.access_token);
+      })
+      .catch((err) => console.log(err));
+  };
 
-  export default ClientApp
+  const state = data?.data;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <Fragment>
+        {state && (
+          <div>
+            <h1>Logged in user:</h1>
+            <p>Username: {state.username}</p>
+            <p>Email: {state.email}</p>
+            <p>Is active: {state.isActive.toString()}</p>
+          </div>
+        )}
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              {!state && (
+                <div>
+                  <RegisterForm onSubmit={handleRegister} />
+                </div>
+              )}
+            </div>
+            <div className="col-6">
+              {!state && (
+                <div>
+                  <LoginForm onSubmit={handleLogin} />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
+};
+
+export default ClientApp;
