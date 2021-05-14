8fff2a9929750bfaff0fec0d6264efa27f9d6045
moshfiqrony <moshfiqrony@gmail.com>
2021-05-15 00:14:41 +0600

    MR | Layout updated for register

--- frontend/src/containers/Register.tsx
+++ frontend/src/containers/Register.tsx
@@ -1,51 +1,52 @@
 import React, { useState } from "react";
+import { PrimaryButton } from "../components/Buttons";
 import Input from "../components/Input";
 
 const RegisterForm = ({
-    onSubmit = (username: string, password: string, email: string) => {},
-  }) => {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    return (
-      <div>
-        <h3>Register</h3>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            onSubmit(username, password, email);
-          }}
-        >
-          <div>
-            <p>Username: </p>
-            <Input
-              value={username}
-              type="text"
-              onChange={(e: any) => setUsername(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Email: </p>
-            <Input
-              value={email}
-              type="email"
-              onChange={(e: any) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <p>Password: </p>
-            <Input
-              value={password}
-              type="password"
-              onChange={(e: any) => setPassword(e.target.value)}
-            />
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    );
-  };
+  onSubmit = (username: string, password: string, email: string) => {},
+}) => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div>
+      <h3 className="text-center">Register</h3>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit(username, password, email);
+        }}
+      >
+        <div className='mb-3'>
+          <p>Username: </p>
+          <Input
+            value={username}
+            type="text"
+            onChange={(e: any) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='mb-3'>
+          <p>Email: </p>
+          <Input
+            value={email}
+            type="email"
+            onChange={(e: any) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className='mb-3'>
+          <p>Password: </p>
+          <Input
+            value={password}
+            type="password"
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='text-center'>
+          <PrimaryButton name='Register' type='submit'/>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-  export default RegisterForm
+export default RegisterForm;
