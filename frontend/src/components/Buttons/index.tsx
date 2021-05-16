52565eb0fbb9287d7608d6462029330ed3bd5366
moshfiqrony <moshfiqrony@gmail.com>
2021-05-16 14:05:52 +0600

    MR | Danger button added

--- frontend/src/components/Buttons/index.tsx
+++ frontend/src/components/Buttons/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
 
-interface Button{
-    name: string,
-    type?: "button" | "submit" | "reset"
+interface Button {
+  name: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export const PrimaryButton = (params: Button) => {
-    return<button className='btn btn-primary w-100' type={params.type}>
-        {params.name}
+  return (
+    <button className="btn btn-primary w-100" type={params.type}>
+      {params.name}
     </button>
-}
+  );
+};
+
+export const DangerButton = (params: Button) => {
+  return (
+    <button className="btn btn-danger w-100" onClick={params.onClick} type={params.type}>
+      {params.name}
+    </button>
+  );
+};
